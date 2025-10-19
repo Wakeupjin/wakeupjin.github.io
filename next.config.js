358397c2e1847633f71e8c1b82fa19c94b88fb8a
kangjin lee <kangjinlee@kangjins-MacBook-Pro.local>
2025-10-19 18:04:58 +0900

    Fix GitHub Pages path configuration

--- next.config.js
+++ next.config.js
@@ -4,9 +4,7 @@ const nextConfig = {
   trailingSlash: true,
   images: {
     unoptimized: true
-  },
-  assetPrefix: '/wakeupjin.github.io',
-  basePath: '/wakeupjin.github.io'
+  }
 }
 
 module.exports = nextConfig
